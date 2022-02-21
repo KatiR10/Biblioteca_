5e5e0ffc6d32d5c91419cae6985344cde1d25de9
KatiR10 <katiareder5@gmail.com>
2022-02-20 23:13:41 -0300

    Resolução dos Problemas

--- Biblioteca/Models/FiltrosEmprestimos.cs
+++ Biblioteca/Models/FiltrosEmprestimos.cs
@@ -1,10 +1,9 @@
-using System;
 
 namespace Biblioteca.Models
 {
     public class FiltrosEmprestimos
     {
-        public string TipoFiltro {get; set;}
-        public string Filtro {get; set;}
+        public string TipoFiltro { get; set; }
+        public string Filtro { get; set; }
     }
 }
